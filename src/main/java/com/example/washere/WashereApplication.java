e46e861e860b9a0b1e9b7f8faac8dc8d661e8e7b
ch7895 <changkyu7895@gmail.com>
2022-05-10 17:30:16 +0900

    'hello'

---
+++ src/main/java/com/example/washere/WashereApplication.java
@@ -0,0 +1,13 @@
+package com.example.washere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WashereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/washere/WashereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.washere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WashereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
