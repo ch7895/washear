2cf0a0c81359ef0c7bac5e638cf424b7ee0e56c8
chkyu7895 <changkyu.kim1@cj.net>
2022-09-02 00:10:47 +0900

    20220902

---
+++ src/test/java/com/example/washere/Chap7.java
@@ -0,0 +1,73 @@
+package com.example.washere;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.junit.Before;
+
+import org.junit.Test;
+
+import java.util.stream.Stream;
+
+
+@Slf4j
+public class Chap7 {
+
+    
+    @Before //  
+//    @BeforeClass // once 
+    public void setup (){
+        log.debug("start");
+    
+    }
+
+    @Test 
+    public void testStream1(){
+
+        long startTime = System.currentTimeMillis();
+        
+        Long n= Stream.iterate(1L, i -> i + 1)
+                         .limit(1000)
+                         .reduce(0L, Long::sum);
+        
+        long endTime = System.currentTimeMillis();
+        log.info("testStrema1 {}",endTime-startTime);
+
+        System.out.println(n);
+    }
+
+
+    @Test 
+    public void testForLoop1(){
+
+        long startTime = System.currentTimeMillis();
+        
+        int sum=0;
+        for(int i=0; i<=1000; i++){
+            sum+=i;
+        }
+
+        long endTime = System.currentTimeMillis();
+        log.info("test1ForLoop1 {}",endTime-startTime);
+
+        System.out.println(sum);
+    }
+
+
+    @Test 
+    public void testStream2(){
+
+        long startTime = System.currentTimeMillis();
+        
+        Long n= Stream.iterate(1L, i -> i + 1)
+                         .limit(1000)
+                         .parallel()
+                         .reduce(0L, Long::sum);
+        
+        long endTime = System.currentTimeMillis();
+        log.info("testStrema2 {}",endTime-startTime);
+
+        System.out.println(n);
+    }
+
+
+}
